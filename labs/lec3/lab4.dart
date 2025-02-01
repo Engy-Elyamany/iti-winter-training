a0664761482aa7a7c09cbfbf067189a3e7573c0e
Engy-Elyamany <engyelyamany26@gmail.com>
2025-02-02 00:55:13 +0200

    "lec3 - lab4"

--- labs/lec3/lab4.dart
+++ labs/lec3/lab4.dart
@@ -5,7 +5,7 @@ nanoseconds, where each argument has the default value of zero.
 
 • Implement two member functions: toSecondOfDayand 
 toNanoOfDayreturning the number of seconds and nanoseconds 
-accordingly passed from the beginning of the da
+accordingly passed from the beginning of the day.
 */
 
 /* Example
@@ -19,7 +19,7 @@ class simpleTime {
   simpleTime([this.h = 0, this.m = 0, this.s = 0, this.nano = 0]);
 
   int toSecondOfDay() {
-    int res = h * 3600 + m * 60 + s + nano*1000000000;
+    int res = h * 3600 + m * 60 + s + nano * 1000000000;
     return res;
   }
 
@@ -31,14 +31,14 @@ class simpleTime {
 
 void main() {
   var noon = simpleTime(12);
-  print(noon.toSecondOfDay());                 //43200
+  print(noon.toSecondOfDay()); //43200
 
   var halfPastSeven = simpleTime(6, 30);
-  print(halfPastSeven.toNanoOfDay());         //23400000000000
+  print(halfPastSeven.toNanoOfDay()); //23400000000000
 
   var nanosecond = simpleTime(0, 0, 0, 1);
-  print(nanosecond.toNanoOfDay());           // 1
+  print(nanosecond.toNanoOfDay()); // 1
 
   var second = simpleTime(0, 0, 1);
-  print(second.toNanoOfDay());              // 1000000000
+  print(second.toNanoOfDay()); // 1000000000
 }
