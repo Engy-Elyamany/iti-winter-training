447a04d33d7d9d9a05890b1c04c875259ca75138
Engy Elyamany <157080761+Engy-Elyamany@users.noreply.github.com>
2025-02-08 12:44:00 +0200

    Delete labs/lec5/On boarding directory

--- labs/lec5/On boarding/lib/resources/imageManager.dart
+++
@@ -1,7 +0,0 @@
-class image_manager {
-  image_manager._();
-
-  static const String img1 = 'images/img1.png';
-  static const String img2 = 'images/img2.png';
-  static const String img3 = 'images/img3.png';
-}
